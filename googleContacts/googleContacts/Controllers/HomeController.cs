7f11c7ede9c2bdc80b7791d8296c6e96af262f31
Rohit Vyavahare <rohit.vyavahare@gmail.com>
2015-04-28 11:32:09 -0700

    Initial

---
+++ googleContacts/googleContacts/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.GData.Extensions.Apps;
+using Google.GData.Client;
+using Google.Contacts;
+using System.Net;
+using System.Text;
+
+namespace googleContacts.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+
+
+            string str;
+            string clientId = "";
+            string clientSecret = "";
+            string redirectUri = "";
+            string scopes = "https://www.google.com/m8/feeds/";
+            string url = null ;
+
+            OAuth2Parameters parameters = new OAuth2Parameters
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                RedirectUri = redirectUri,
+                Scope = scopes,
+                AccessType = "offline",
+                ApprovalPrompt = "force"
+
+            };
+
+            if ((str = Request["code"]) != null)
+            {
+
+                parameters.AccessCode = str;
+                OAuthUtil.GetAccessToken(parameters);
+                System.IO.File.WriteAllText(@"C:\Users\rohit\temp.txt", parameters.RefreshToken);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Access_Token:");
+                sb.Append(parameters.AccessToken);
+                ViewBag.Message = sb.ToString();
+                return View();
+            }
+            else
+            {
+                string text = null;
+                try
+                {
+                    text = System.IO.File.ReadAllText(@"C:\Users\rohit\temp.txt");
+                }
+                catch (Exception e)
+                {
+
+                }
+                finally
+                {
+                    if (text == null)
+                    {
+                       url = OAuthUtil.CreateOAuth2AuthorizationUrl(parameters);
+                    }
+                    else
+                    {
+                        parameters.RefreshToken = text;
+                        OAuthUtil.RefreshAccessToken(parameters);
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("Access_Token:");
+                        sb.Append(parameters.AccessToken);
+                        url = sb.ToString();
+                    }
+                    
+                    ViewBag.Message = url;
+                    
+                }
+                return View();
+            }
+            
+            
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ googleContacts/googleContacts/Scripts/Google.js
@@ -0,0 +1,56 @@
+﻿// Google Importation stuff
+
+var googleContacts = [];
+
+window.onload = function () {
+
+    var ele = document.getElementById("myInput");
+    var choice =  ele.value;
+    console.log(choice);
+    
+    if (choice.indexOf("Access_Token") > -1) {
+
+        var ele2 = document.getElementById("glogin");
+        ele2.style.visibility = 'hidden';;
+        console.log(choice);
+        var split = choice.split(":");
+        console.log(split[1]);
+        $.get("https://www.google.com/m8/feeds/contacts/default/full?alt=json&access_token=" +split[1] + "&max-results=11700&v=3.0",
+            handleGoogleContacts);
+    }
+    
+    }
+  
+function handleGoogleContacts(response) {
+    if (response.feed.entry.length > 0) {
+        for (var i = 0 ; i < response.feed.entry.length ; i++) {
+
+            if (response.feed.entry[i].gd$email) {
+
+                if (response.feed.entry[i].gd$email[0] != undefined) {
+
+                    var tempEmail = response.feed.entry[i].gd$email[0].address;
+
+                    if (response.feed.entry[i].gd$name != undefined) {
+
+                        googleContacts.push({
+                            "label": response.feed.entry[i].gd$name.gd$fullName.$t,
+                            "value": tempEmail
+                        });
+                    }
+                    else {
+                        googleContacts.push({
+                            "label": tempEmail,
+                            "value": tempEmail
+                        });
+                    }
+                }
+            }
+            else {
+                continue;
+            }
+        }
+        console.log( JSON.stringify(googleContacts));
+    }
+    else { alert("something went wrong ! your contact list is undefined"); }
+}
